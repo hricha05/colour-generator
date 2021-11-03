b8b7797a9377410f48ab16f7f422472515a3fb42
hricha05 <harun.richards@outlook.com>
2021-11-03 22:45:15 +0000

    build-1

---
+++ app.js
@@ -0,0 +1,33 @@
+document
+	.getElementById('click-me')
+	.addEventListener('click', hexFormatGenerator);
+document
+	.getElementById('simple')
+	.addEventListener('click', textFormatGenerator);
+document.getElementById('hex').addEventListener('click', hexFormatGenerator);
+const colorText = document.getElementById('color');
+colorText.style.color = hexFormatGenerator();
+colorText.innerHTML = hexFormatGenerator();
+
+function clickMe() {
+	console.log('click');
+}
+
+function hexFormatGenerator() {
+	const hex = '0123456789ABCDEF'.split('');
+	let color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += hex[Math.floor(Math.random() * 16)];
+	}
+	console.log(color);
+	return color;
+}
+
+function textFormatGenerator() {
+	console.log('simple text');
+}
+
+function myColor() {
+	const el = document.getElementById('color');
+	el.ontoggle;
+}
